591bf77ef3fc47c501f67011fc948b3ab4c2282e
Makrand <makrandsanghvi124@gmail.com>
2026-05-27 23:47:55 +0530

    Final production API setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,19 @@
-
 import axios from "axios";
-
 const API = axios.create({
-
-  baseURL:
-    "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 
 });
 
+
 /* ================= TOKEN INTERCEPTOR ================= */
-
 API.interceptors.request.use(
-
   (config) => {
-
-    const token =
-      localStorage.getItem("token");
-
+    const token = localStorage.getItem("token");
     if (token) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`;
-
+      config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   }
-
 );
-
 export default API;
 
